81e2265ea26f44e20e063d68308d2fd1e90e5a22
run-after <run.after@protonmail.com>
2021-06-01 09:36:33 -0500

    Add Message Schema

---
+++ models/Message.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Shema = mongoose.Schema;
+
+const MessageSchema = new Schema(
+  {
+    title: { type: String, required: true, maxLength: 50 },
+    body: { type: String, required: true, maxLength: 200 },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
+  }, {timestamps: true}
+);
+
+module.exports = mongoose.model('Message', MessageSchema);

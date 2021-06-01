6f0ee9d0d866979a1e9008f28d782e5297071068
run-after <run.after@protonmail.com>
2021-06-01 10:44:56 -0500

    Add get/post routes for sign up form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/userController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/sign-up', userController.sign_up_form_get);
+
+router.post('/sign-up', userController.sign_up_form_post)
 
 module.exports = router;

1675664bf2a3d28238d73c57c617bd2ec949aa0c
run-after <run.after@protonmail.com>
2021-06-02 13:13:21 -0500

    Add login routes get/post

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', user: req.user });
 });
 
 router.get('/sign-up', userController.sign_up_form_get);
@@ -14,5 +14,9 @@ router.post('/sign-up', userController.sign_up_form_post);
 router.get('/confirm', userController.confirm_membership_get);
 
 router.post('/confirm', userController.confirm_membership_post);
+
+router.get('/login', userController.login_get);
+
+router.post('/login', userController.login_post);
 
 module.exports = router;

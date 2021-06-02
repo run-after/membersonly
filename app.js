24c08a45a9c932c5dd1e0e8737d433eacc7337f3
run-after <run.after@protonmail.com>
2021-06-02 13:12:55 -0500

    Add passport and express session middleware

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 
 var mongoose = require('mongoose');
 var mongoDB = process.env.MONGODB_URI;
@@ -19,6 +24,40 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) { return done(err); };
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      };
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'Incorrect password' });
+        };
+      });
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(logger('dev'));
 app.use(express.json());
